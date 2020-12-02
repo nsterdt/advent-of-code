1d297e367e6de4c6d94fa6187dd06f3324b55175
nsterdt <nico-sterdt@web.de>
2020-12-02 09:06:48 +0100

    solution second day, part 1

--- day_2/lib/day2.ex
+++ day_2/lib/day2.ex
@@ -1,18 +1,39 @@
 defmodule Day2 do
-  @moduledoc """
-  Documentation for `Day2`.
-  """
 
-  @doc """
-  Hello world.
+  alias Day2.PasswordPolicy
 
-  ## Examples
+  def part1 do
+    open_input()
+    |> parse_to_list
+    |> Enum.map(&parse_to_password_policy/1)
+    |> Enum.count(&is_valid_password/1)
+  end
 
-      iex> Day2.hello()
-      :world
+  def open_input() do
+    {:ok, content} = File.read("input.txt")
+    content
+  end
 
-  """
-  def hello do
-    :world
+  def parse_to_list(content) do
+    String.split(content, "\n")
+  end
+
+  def parse_to_password_policy(password_line) do
+    [minimum, rest] = String.split(password_line, "-")
+    [maximum, rest] = String.split(rest, " ", parts: 2)
+    [letter, rest] = String.split(rest, ":")
+    password = String.trim(rest)
+
+    %PasswordPolicy{minimum: String.to_integer(minimum),
+                    maximum: String.to_integer(maximum),
+                    letter: letter,
+                    password: password}
+  end
+
+  def is_valid_password(%PasswordPolicy{minimum: minimum, maximum: maximum, letter: letter, password: password}) do
+    password = String.graphemes(password)
+    letter_count = Enum.count(password, fn(char) -> char == letter end)
+
+    minimum <= letter_count and maximum >= letter_count
   end
 end
---
+++ day_2/lib/password.ex
@@ -0,0 +1,6 @@
+defmodule Day2.PasswordPolicy do
+  defstruct minimum: nil,
+            maximum: nil,
+            letter: nil,
+            password: nil
+end
